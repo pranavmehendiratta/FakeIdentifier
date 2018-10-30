ade366c51c4d21f687ecc8e818ac614aa2dc39a6
Pranav Mehendiratta <mehendiratta@wisc.edu>
2018-10-29 23:17:43 -0500

    Done parsing the image using tesseract

--- main.py
+++ main.py
@@ -3,20 +3,35 @@ import pytesseract
 import argparse
 import cv2
 import os
+import sys
 
-def main():
+def main(argv):
     print("Inside main")
-    extractText()
+    #print(type(argv))
+    #print(' '.join(argv)) 
+
+    if len(argv) < 2:
+	print('image should be paramter')
+	sys.exit(1)
+
+    # Extract the text from the drivers license
+    extractText(argv[1])
 
 def checkIfAddressIsValid(addr):
     # addr will be a dictionary
     print("Inside checkIfAddressIsValid")
 
-def extractText():
+def extractText(imagePath):
     # Use tesseract to extract the text from the image
     print("Inside extract text using tesseract");
     
+    # Load image
+    print("Image path: " + imagePath)
 
+    # Convert the image to text
+    image = Image.open(imagePath)
+    text = pytesseract.image_to_string(image)
+    print(text);
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
