816c5de6a9363436ac9e2a3760c6c7c49ea900e2
Pranav Mehendiratta <mehendiratta@wisc.edu>
2018-10-29 22:48:41 -0500

    Initialized the project for image-text recognition

---
+++ main.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import os
+
+def main():
+    print("Inside main")
+    extractText()
+
+def checkIfAddressIsValid(addr):
+    # addr will be a dictionary
+    print("Inside checkIfAddressIsValid")
+
+def extractText():
+    # Use tesseract to extract the text from the image
+    print("Inside extract text using tesseract");
+    
+
+
+if __name__ == '__main__':
+    main()
