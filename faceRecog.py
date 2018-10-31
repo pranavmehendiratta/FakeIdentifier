0b5012c98a9996d13b190831dc7e87b2a9ae1579
Utkarsh Jain <utkarsh.jain1796@gmail.com>
2018-10-30 21:24:08 -0500

    Facial Recognition

--- faceRecog.py
+++ faceRecog.py
@@ -1,8 +1,6 @@
 import face_recognition
 from PIL import Image, ImageDraw
 
-# This is an example of running face recognition on a single image
-# and drawing a box around each person that was identified.
 
 # Load a sample picture and learn how to recognize it.
 obama_image = face_recognition.load_image_file("capture.png")
@@ -29,8 +27,6 @@ unknown_image = face_recognition.load_image_file("together.png")
 face_locations = face_recognition.face_locations(unknown_image)
 face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
 
-# Convert the image to a PIL-format image so that we can draw on top of it with the Pillow library
-# See http://pillow.readthedocs.io/ for more about PIL/Pillow
 pil_image = Image.fromarray(unknown_image)
 # Create a Pillow ImageDraw Draw instance to draw with
 draw = ImageDraw.Draw(pil_image)
@@ -57,8 +53,6 @@ for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodi
     draw.text((left + 6, bottom - text_height - 5), name, fill=(255, 255, 255, 255))
 
 
-# Remove the drawing library from memory as per the Pillow docs
 del draw
 
-# Display the resulting image
 pil_image.show()
